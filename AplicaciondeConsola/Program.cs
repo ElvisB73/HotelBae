0c1d6787e4b84a2e6ad4486824c38ce8659bf4d1
Baez73 <eb2024-3416@uce.edo.do>
2026-05-23 18:07:31 -0400

    aplicacion.testes

---
+++ AplicaciondeConsola/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Hotel.Negocio_.Modelo;
+using Hotel.Negocio_.Servicios;
+
+
+namespace HotelZormat.testes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var servicio = new Reservaservice();
+
+            // Aquí llamaremos los métodos de cada reto para probarlos
+
+            Console.WriteLine("\n--- Tests finalizados ---");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Hotel.Negocio_/Servicios/Reservaservice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Negocio_.Servicios
+{
+    public class Reservaservice
+    {
+        public bool ValidarTipoHabitacion(string tipo)
+        {
+            if (string.IsNullOrWhiteSpace(tipo))
+            {
+                return false;
+            }
+
+            if (tipo == "Sencilla" || tipo == "Doble" || tipo == "Suite")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
